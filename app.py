3cfa979a8b2d58545b867f80b7df545ec4794a05
syc119 <wssrgrrr@gmail.com>
2026-04-23 04:56:01 +0000

    0423.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Docker Flask!"
+    return """<html>
+            <head>
+                <title>首頁</title>
+            </head>
+            <body>
+                <h1>歡迎來到我的網站</h1>
+                <p>這是用 Flask 直接回傳 HTML</p>
+
+                <a href="/about">前往 About</a>
+            </body>
+        </html>"""
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000,debug=True)
