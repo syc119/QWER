281c25a240b5f55b6402c924c88d382fd04a07b0
syc119 <wssrgrrr@gmail.com>
2026-04-23 05:02:07 +0000

    0423-2.

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """<html>
-            <head>
-                <title>首頁</title>
-            </head>
-            <body>
-                <h1>歡迎來到我的網站</h1>
-                <p>這是用 Flask 直接回傳 HTML</p>
-
-                <a href="/about">前往 About</a>
-            </body>
-        </html>"""
+   return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000,debug=True)
